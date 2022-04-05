1c8b1fac7946cbab354b91e5eafd2725ad1a399e
Mahmoud Mohamed Labib <87601839+MahmoudLabib1@users.noreply.github.com>
2022-04-05 16:40:23 +0200

    Add files via upload

---
+++ Ex_15/Ex_15.c
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ *
+ * [Description]:  Write a program to display English alphabets from A to Z.
+ *
+ * [File Name]: Ex_15.c
+ *
+ * [Difficulty]: basic Level
+ *
+ * [Author]: Mahmoud_Labib
+ *
+ *******************************************************************************/
+
+#include <stdio.h>
+
+int clcPower(int num, int power){
+	int result=1;
+	while(power != 0){
+		result *= num;
+		--power;
+	}
+
+	return result;
+}
+int main(int argc, char *argv[])
+{
+	int num , power;
+	printf("Enter the Number And Power\n");
+	printf("Enter the Number :");
+	scanf("%d",&num);
+	printf("\nEnter the power : ");
+	scanf("%d",&power);
+
+	printf("\n The resylt is : %d\n",clcPower(num, power));
+
+	system("pause");   //To make the terminal not closed after executing the code
+	return 0;
+}
